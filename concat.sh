a4f0d0c171c9c1c34b3f8ab2c3ba8328aeef3a07
Vladimir Bezhenar <me@vbezhenar.com>
2023-09-09 14:41:44 +0600

    Add concat script

---
+++ concat.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+cat <<'EOF'
+_на текущий момент статья находится в процессе написания, опубликованы 2/7 частей_
+
+Все файлы можно взять [тут](https://github.com/vbezhenar/stm32-tutorial).
+
+EOF
+
+tail -n +2 README.md
+
+cat <<'EOF'
+
+---
+
+EOF
+
+cat 1-explore/README.md
+
+cat <<'EOF'
+
+---
+
+EOF
+
+cat 2-loop/README.md
